83785875377203094ebf269cc313e661a9bd4597
do6roetyro <do6roeytro@yandex.ru>
2023-12-12 17:35:27 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-// -- global
+// -- GLOBAL
 import "./styles/main.scss";
 import "./styles/fonts.scss";
 import "./styles/mixins.scss";
 import "./styles/page.scss";
 import "./styles/button.scss";
-// -- header
+// -- HEADER
 import "./styles/main-nav.scss";
-// -- main
+// -- MAIN
 import "./styles/main-container.scss";
 import "./styles/hero.scss";
 import "./styles/hero-title.scss";
@@ -14,8 +14,11 @@ import "./styles/collaboration.scss";
 import "./styles/new.scss";
 import "./styles/new-link.scss";
 import "./styles/promo.scss";
-// --footer
-
-
-
-
+import "./styles/promo-title.scss";
+import "./styles/favourite.scss";
+import "./styles/favourite-link.scss";
+import "./styles/download.scss";
+import "./styles/download-container.scss";
+//-- mobile_app
+import "./styles/mobile-app.scss";
+// --FOOTER
